db90df8e582f6980d86f5d378c85b8a0687b9b6e
Chris <christopherbuckley71@gmail.com>
2020-09-29 17:20:03 +0100

    First Commit of the OOPLabsheets3+ Project

---
+++ Lab3/exercise2/TestComputer.java
@@ -0,0 +1,31 @@
+package Lab3.exercise2;
+
+import javax.swing.*;
+
+public class TestComputer {
+    public static void main(String[] args) {
+        String output = "";
+
+        Computer c1 = new Computer();
+
+        output += "Calling the no argument Computer constructor. " +
+                "The first Computer object details are \n\n" + c1.toString();
+
+        Computer c2 = new Computer("Dell","Laptop",3.25,16,550.99);
+
+        c2.setSpeed(-3.15);
+
+        Computer c3 = new Computer("AMD","Desktop",2.9,12,865.00);
+
+        c3.setRAM(-8);
+
+        output += "\n\nCalling the multi argument Computer constructor."+
+                "the second computer object details are:\n\n"+c2.toString()+
+                "\n\nthe third computer object details are:\n\n"+c3.toString();
+
+        JOptionPane.showMessageDialog(null,output,"computer object data",JOptionPane.INFORMATION_MESSAGE);
+        System.exit(0);
+
+
+    }
+}
